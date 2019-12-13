5f3d8c2fd66c1ace2d5731c06b6a503d94d7a724
nelavallisaikrishna <saikrishna@capitalquotient.com>
2019-12-13 18:24:30 +0530

    Passing multiple arguments into functions and returing multiple values

--- cards/deck.go
+++ cards/deck.go
@@ -25,3 +25,7 @@ func (d deck) print() {
 		fmt.Println(i, card)
 	}
 }
+
+func deal(d deck, inhand int)(deck, deck){
+	return d[:inhand], d[inhand:]
+}
--- cards/main.go
+++ cards/main.go
@@ -1,15 +1,13 @@
 package main
 
-import "fmt"
-
 func main() {
 	// var card string = "Ace of Spades"
 	// card := "Ace of Spades"   /* variable initialization*/
 	// card = "Five of Diamonds" /* variable assign value*/
 	// card := newCard()
 	cards := newDeck()
-	fmt.Println("cards --------------", cards)
-	cards.print()
+	hand, remainingCards := deal(cards, 5)
+	hand.print()
+	remainingCards.print()
 
 }
-
