1dd1a6a6a13f994c4e60976072dee9a68a47809e
nelavallisaikrishna <saikrishna@capitalquotient.com>
2019-12-13 10:56:19 +0530

    assiging and initillazation of varibles

---
+++ cards/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var card string = "Ace of Spades"
+	card := "Ace of Spades"   /* variable initialization*/
+	card = "Five of Diamonds" /* variable assign value*/
+	fmt.Println("card --------------", card)
+
+}
