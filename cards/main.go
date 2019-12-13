f22ef1a46d6eaabe8aff34e1bb77f6d3bacf7c58
nelavallisaikrishna <saikrishna@capitalquotient.com>
2019-12-13 11:07:07 +0530

    function and return types

--- cards/main.go
+++ cards/main.go
@@ -4,8 +4,13 @@ import "fmt"
 
 func main() {
 	// var card string = "Ace of Spades"
-	card := "Ace of Spades"   /* variable initialization*/
-	card = "Five of Diamonds" /* variable assign value*/
+	// card := "Ace of Spades"   /* variable initialization*/
+	// card = "Five of Diamonds" /* variable assign value*/
+	card := newCard()
 	fmt.Println("card --------------", card)
 
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
