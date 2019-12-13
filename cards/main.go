cd858216d0a09b9960ffde264b4d7aa93554026a
nelavallisaikrishna <saikrishna@capitalquotient.com>
2019-12-13 11:34:00 +0530

    create slice(array), append, for loop

--- cards/main.go
+++ cards/main.go
@@ -6,8 +6,13 @@ func main() {
 	// var card string = "Ace of Spades"
 	// card := "Ace of Spades"   /* variable initialization*/
 	// card = "Five of Diamonds" /* variable assign value*/
-	card := newCard()
-	fmt.Println("card --------------", card)
+	// card := newCard()
+	cards := []string{"Ace of Diamonds", newCard()}
+	cards = append(cards,"Six of Diamonds")
+	fmt.Println("cards --------------", cards)
+	for i, card := range cards{
+		fmt.Println(i, card)
+	}
 
 }
 
