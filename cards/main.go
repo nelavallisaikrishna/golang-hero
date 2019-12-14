a72f5cf3024082ff6f698d33581f939ef9d6763c
nelavallisaikrishna <saikrishna@capitalquotient.com>
2019-12-14 16:34:54 +0530

    Converting to bytes

--- cards/main.go
+++ cards/main.go
@@ -1,13 +1,20 @@
 package main
 
+import "fmt"
+
 func main() {
 	// var card string = "Ace of Spades"
 	// card := "Ace of Spades"   /* variable initialization*/
 	// card = "Five of Diamonds" /* variable assign value*/
 	// card := newCard()
-	cards := newDeck()
-	hand, remainingCards := deal(cards, 5)
-	hand.print()
-	remainingCards.print()
+	// cards := newDeck()
+	//calling function with multiple return values
+	// hand, remainingCards := deal(cards, 5)
+	// hand.print()
+	// remainingCards.print()
+
+	call := "Hi! How are you?"
+	//converting to bytes
+	fmt.Println([]byte(call))
 
 }
