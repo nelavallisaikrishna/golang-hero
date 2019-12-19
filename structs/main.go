719791647cf8063ff7016f63f11cbcd279d34307
nelavallisaikrishna <saikrishna@capitalquotient.com>
2019-12-19 20:28:16 +0530

    changing value without using &

--- structs/main.go
+++ structs/main.go
@@ -2,17 +2,41 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
 	// sai := person{"saikrishna","nelavalli"}
 	// sai := person{firstName: "saikrishna", lastName: "nelavalli"}
-	var sai person
-	sai.firstName = "saikrishna"
-	sai.lastName = "Nelavalli"
-	fmt.Println(sai)
-	fmt.Printf("%+v", sai)
+	// var sai person
+	// sai.firstName = "saikrishna"
+	// sai.lastName = "Nelavalli"
+	sai := person{
+		firstName: "saikrishna",
+		lastName:  "Nelavalli",
+		contact: contactInfo{
+			email:   "sai@gmail.com",
+			zipCode: 524126,
+		},
+	}
+	// saiPointer := &sai
+	sai.updateName("sai")
+	sai.print()
+}
+
+func (pointerToSai *person) updateName(newFirstName string) {
+	(*pointerToSai).firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+
 }
