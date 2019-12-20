1e13b8e6f8071de64fb5541c465a5cf060c86669
nelavallisaikrishna <saikrishna@capitalquotient.com>
2019-12-20 15:18:47 +0530

    creating map in different types

--- map/main.go
+++ map/main.go
@@ -3,10 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		"red":   "#ff0000",
-		"black": "#000000",
-	}
 
+	// colors := map[string]string{
+	// 	"red":   "#ff0000",
+	// 	"black": "#000000",
+	// }
+
+	// or
+
+	// var colors map[string]string
+	// colors := make(map[string]string)
+	// colors["white"] = "#ffffff"
+	colors := make(map[int]string)
+	colors[10] = "#ffffff"
 	fmt.Println(colors)
+
+	//Delete map key value by passing map name and key name
+	delete(colors, 10)
+	fmt.Println("after delete", colors)
 }
