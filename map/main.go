46b0dfa44bab5f426e894de3929c85e8b7fab095
nelavallisaikrishna <saikrishna@capitalquotient.com>
2019-12-20 15:35:23 +0530

    looping map

--- map/main.go
+++ map/main.go
@@ -4,21 +4,30 @@ import "fmt"
 
 func main() {
 
-	// colors := map[string]string{
-	// 	"red":   "#ff0000",
-	// 	"black": "#000000",
-	// }
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"black": "#000000",
+		"white": "#ffffff",
+	}
+
+	printMap(colors)
 
 	// or
 
 	// var colors map[string]string
 	// colors := make(map[string]string)
 	// colors["white"] = "#ffffff"
-	colors := make(map[int]string)
-	colors[10] = "#ffffff"
-	fmt.Println(colors)
+	// colors := make(map[int]string)
+	// colors[10] = "#ffffff"
+	// fmt.Println(colors)
 
-	//Delete map key value by passing map name and key name
-	delete(colors, 10)
-	fmt.Println("after delete", colors)
+	// //Delete map key value by passing map name and key name
+	// delete(colors, 10)
+	// fmt.Println("after delete", colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println(color, hex)
+	}
 }
