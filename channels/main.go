92fec1c4ca5bf4b8e767b2a48289a4b8885eae07
nelavallisaikrishna <saikrishna@capitalquotient.com>
2019-12-24 11:56:42 +0530

    Function Literal

--- channels/main.go
+++ channels/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -14,13 +15,22 @@ func main() {
 		"http://amazon.com",
 	}
 
-	c := make(chan string)  // create channel type
+	c := make(chan string) // create channel type
 
 	for _, link := range links {
 		go checkLinks(link, c)
 	}
-	for i := 0; i < len(links); i++ {   // Receiving the respone depending upon channnel length
-		fmt.Println(<-c)
+	// for i := 0; i < len(links); i++ {   // Receiving the respone depending upon channnel length
+	// 	fmt.Println(<-c)
+	// }
+
+	// function Literals
+
+	for l := range c {
+		go func(link string) { // creating literal function
+			time.Sleep(5 * time.Second)
+			checkLinks(link, c)
+		}(l)
 	}
 
 }
@@ -30,9 +40,9 @@ func checkLinks(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "is down")
-		c <- "Might be down"
+		c <- link
 		return
 	}
 	fmt.Println(link, "is up!")
-	c <- "Is up"
+	c <- link
 }
